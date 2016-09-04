c5a3bbf9d5ffd05f03588e0c84a0a5b193e0d184
JishnuM <mohanjishnu@gmail.com>
2016-09-04 08:44:26 +0800

    Build title change on user view

--- user-view-app/build/bundled/service-worker.js
+++ user-view-app/build/bundled/service-worker.js
@@ -29,7 +29,7 @@
 
 
 /* eslint-disable quotes, comma-spacing */
-var PrecacheConfig = [["/index.html","078725ca6200e95048926be6d4f8e514"],["/src/user-view/user-view.html","7d3743178f8cde12e01676ac86b6febe"]];
+var PrecacheConfig = [["/index.html","078725ca6200e95048926be6d4f8e514"],["/src/user-view/user-view.html","8df7517aa0fffe36987695d89d85eb89"]];
 /* eslint-enable quotes, comma-spacing */
 var CacheNamePrefix = 'sw-precache-v1--' + (self.registration ? self.registration.scope : '') + '-';
 
